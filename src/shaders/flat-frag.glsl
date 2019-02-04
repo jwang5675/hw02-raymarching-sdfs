e619ae0907680cb30c2d133aca1a32d94c9ae3b7
Jason Wang <jwang5675@gmail.com>
2019-02-04 11:13:27 -0500

    finished part 1, ray casting

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -8,6 +8,17 @@ uniform float u_Time;
 in vec2 fs_Pos;
 out vec4 out_Col;
 
+float fov = 90.0 * 3.14159 / 180.0;
+
 void main() {
-  out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
+	vec3 u_Right = normalize(cross(u_Ref - u_Eye, u_Up));
+	float len = length(u_Ref - u_Eye);
+	float aspectRatio = u_Dimensions.x / u_Dimensions.y;
+	vec3 v = tan(fov / 2.0) * len * u_Up;
+	vec3 h = aspectRatio * tan(fov / 2.0) * len * u_Right;
+	vec3 point = u_Ref + fs_Pos.x * h + fs_Pos.y * v;
+	vec3 ray_direction = normalize(point - u_Eye);
+
+	vec3 color = 0.5 * (ray_direction + vec3(1.0, 1.0, 1.0));
+  out_Col = vec4(color, 1.0);
 }
